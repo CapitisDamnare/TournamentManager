de93fdeca6e74645ceec4781e30a1103b07ec966
CapitisDamnare <capitisdamnare@gmail.com>
2018-03-19 18:46:34 +0100

    Started on the Visu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,39 @@
 package sample;
 
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML private Label rdyLabel;
+    @FXML
+    private Label rdyLabel;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Button btnMode;
+
+    @FXML
+    private Button btnGroupSettings;
+
+    @FXML
+    private TableView<?> vrTableView;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private TreeView<?> menuTree;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -20,5 +43,20 @@ public class Controller implements Initializable {
 
     public void setRdyLabel(String text) {
         rdyLabel.setText(text);
+    }
+
+    private void setExampleData() {
+
+    }
+
+
+    @FXML
+    void btnGroupSettingsOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnModeOnClick(ActionEvent event) {
+
     }
 }
