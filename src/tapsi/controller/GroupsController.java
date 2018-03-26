1fdac79a1eb4cc0e32021001548fcd6224d2181f
CapitisDamnare <capitisdamnare@gmail.com>
2018-03-26 15:45:01 +0200

    Edit on ListView

--- src/tapsi/controller/GroupsController.java
+++ src/tapsi/controller/GroupsController.java
@@ -3,11 +3,15 @@ package tapsi.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import tapsi.Main;
@@ -41,6 +45,74 @@ public class GroupsController implements Initializable, ControllerInterface {
 
     private void setupListView() {
         groupListView.setEditable(true);
+        groupListView.setCellFactory(new Callback<ListView<Team>, ListCell<Team>>() {
+            @Override
+            public ListCell<Team> call(ListView<Team> param) {
+                return new TFListCell();
+            }
+        });
+
+
+    }
+
+    class TFListCell extends ListCell<Team> {
+
+        private TextField textField;
+
+        @Override
+        public void startEdit() {
+            if (!isEditable() || !getListView().isEditable()) {
+                return;
+            }
+            super.startEdit();
+
+            if (isEditing()) {
+                if (textField == null) {
+                    textField = new TextField(getItem().getName());
+                    textField.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent event) {
+                            commitEdit( new Team(textField.getText()));
+                        }
+                    });
+                }
+                textField.setOnKeyPressed(keyEvent -> {
+                    if (keyEvent.getCode() == KeyCode.ENTER) {
+                        setText(getString());
+                        setGraphic(null);
+                    }
+                });
+            }
+
+            textField.setText(getItem().getName());
+            setText(null);
+
+            setGraphic(textField);
+            textField.selectAll();
+        }
+
+        private String getString() {
+            return getItem() == null ? "" : getItem().getName();
+        }
+
+        @Override
+        public void updateItem(Team item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (isEmpty()) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                if (!isEditing()) {
+                    if (textField != null) {
+                        setText(textField.getText());
+                    } else {
+                        setText(item.getName());
+                    }
+                    setGraphic(null);
+                }
+            }
+        }
     }
 
     private void exampleData () {
