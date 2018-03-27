77e65cd3404115cf8316efe3e33d24e9cf60fa82
CapitisDamnare <capitisdamnare@gmail.com>
2018-03-27 22:37:03 +0200

    learning javafx

--- src/tapsi/controller/GroupsController.java
+++ src/tapsi/controller/GroupsController.java
@@ -1,5 +1,7 @@
 package tapsi.controller;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,7 +12,7 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.KeyCode;
+import javafx.scene.input.*;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import tapsi.Main;
@@ -24,6 +26,7 @@ public class GroupsController implements Initializable, ControllerInterface {
 
     @FXML
     private ListView<Team> groupListView;
+    private static final DataFormat SERIALIZED_TEAM_TYPE = new DataFormat("application/x-java-serialized");
 
     private Stage stage;
 
@@ -45,7 +48,59 @@ public class GroupsController implements Initializable, ControllerInterface {
 
     private void setupListView() {
         groupListView.setEditable(true);
-        groupListView.setCellFactory(lv -> new TeamListCell());
+        //groupListView.setCellFactory(lv -> new TeamListCell());
+
+        groupListView.setCellFactory(lv -> {
+            ListCell<Team> cell = new TeamListCell();
+
+            cell.setOnDragDetected(event -> {
+                if (!cell.isEmpty()) {
+                    Integer index = cell.getIndex();
+                    Dragboard db = cell.startDragAndDrop(TransferMode.MOVE);
+                    db.setDragView(cell.snapshot(null, null));
+                    ClipboardContent cc = new ClipboardContent();
+                    cc.put(SERIALIZED_TEAM_TYPE, index);
+                    db.setContent(cc);
+                    event.consume();
+                }
+            });
+
+            cell.setOnDragOver(event -> {
+                Dragboard db = event.getDragboard();
+                if (db.hasString()) {
+                    if (cell.getIndex() != ((Integer) db.getContent(SERIALIZED_TEAM_TYPE)).intValue()) {
+                        event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                        event.consume();
+                    }
+                    event.acceptTransferModes(TransferMode.MOVE);
+                }
+            });
+
+            cell.setOnDragDropped(event -> {
+                Dragboard db = event.getDragboard();
+                if (db.hasContent(SERIALIZED_TEAM_TYPE)) {
+                    int draggedIndex = (Integer) db.getContent(SERIALIZED_TEAM_TYPE);
+                    Team draggedPerson = groupListView.getItems().remove(draggedIndex);
+
+                    int dropIndex;
+
+                    if (cell.isEmpty()) {
+                        dropIndex = groupListView.getItems().size();
+                    } else {
+                        dropIndex = cell.getIndex();
+                    }
+
+                    groupListView.getItems().add(dropIndex, draggedPerson);
+
+                    event.setDropCompleted(true);
+                    groupListView.getSelectionModel().select(dropIndex);
+                    event.consume();
+                }
+            });
+
+            return cell ;
+        });
+
     }
 
     private void exampleData () {
